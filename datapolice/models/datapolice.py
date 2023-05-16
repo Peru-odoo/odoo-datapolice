9dec93ab0e949b137b25a27d47ac556ff71b17f8
Marc Wimmer <marc@itewimmer.de>
2023-05-16 11:58:52 +0200

    [main]

--- datapolice/models/datapolice.py
+++ datapolice/models/datapolice.py
@@ -7,24 +7,40 @@ import logging
 
 _logger = logging.getLogger("datapolice")
 
+
 class DataPolice(models.Model):
-    _name = 'data.police'
+    _name = "data.police"
 
     active = fields.Boolean(default=True)
-    name = fields.Char('Name', required=True, translate=True)
-    fetch_expr = fields.Text("Fetch Expr", help="If given then used; return records, otherwise domain is used with model")
-    check_expr = fields.Text('Expression to chec', required=False, help="Input: obj/object - return False for error, return None/True for ok, or raise Exception or return a string")
-    fix_expr= fields.Char('Def to fix error', required=False)
-    model_id = fields.Many2one('ir.model', string="Model", required=True, ondelete='cascade')
+    name = fields.Char("Name", required=True, translate=True)
+    fetch_expr = fields.Text(
+        "Fetch Expr",
+        help="If given then used; return records, otherwise domain is used with model",
+    )
+    check_expr = fields.Text(
+        "Expression to chec",
+        required=False,
+        help="Input: obj/object - return False for error, return None/True for ok, or raise Exception or return a string",
+    )
+    fix_expr = fields.Char("Def to fix error", required=False)
+    model_id = fields.Many2one(
+        "ir.model", string="Model", required=True, ondelete="cascade"
+    )
     enabled = fields.Boolean("Enabled", default=True)
     errors = fields.Integer("Count Errors")
-    domain = fields.Text('Domain')
+    domain = fields.Text("Domain")
     recipients = fields.Char("Mail-Recipients", size=1024)
-    user_ids = fields.Many2many('res.users', string="Recipients (users)")
-    inform_current_user_immediately  = fields.Boolean("Inform current user immediately", default=False)
+    user_ids = fields.Many2many("res.users", string="Recipients (users)")
+    inform_current_user_immediately = fields.Boolean(
+        "Inform current user immediately", default=False
+    )
     last_errors = fields.Text("Last Error Log")
-    cronjob_group_id = fields.Many2one("datapolice.cronjob.group", string="Cronjob Group")
-    trigger_ids = fields.One2many("datapolice.trigger", 'datapolice_id', ondelete='cascade')
+    cronjob_group_id = fields.Many2one(
+        "datapolice.cronjob.group", string="Cronjob Group"
+    )
+    trigger_ids = fields.One2many(
+        "datapolice.trigger", "datapolice_id", ondelete="cascade"
+    )
 
     def toggle_active(self):
         self.active = not self.active
@@ -46,7 +62,7 @@ class DataPolice(models.Model):
 
     @api.model
     def create(self, values):
-        if 'def' in values.keys():
+        if "def" in values.keys():
             raise Exception("Please use checkdef instead of def!!!")
         result = super(DataPolice, self).create(values)
         return result
@@ -54,14 +70,16 @@ class DataPolice(models.Model):
     @api.constrains("recipients")
     def _check_recipients(self):
         for rec in self:
-            recps = rec.recipients or ''
+            recps = rec.recipients or ""
             recps = recps.replace(";", ",")
+
             def convert(x):
                 x = x.strip()
                 return x
+
             recps = list(map(convert, recps.split(",")))
-            recps = ','.join(recps)
-            if recps != (rec.recipients or ''):
+            recps = ",".join(recps)
+            if recps != (rec.recipients or ""):
                 rec.recipients = recps
 
     @api.model
@@ -72,9 +90,7 @@ class DataPolice(models.Model):
             code.append("True")
         code[-1] = "return " + code[-1]
         code = "\n".join(["  " + x for x in code])
-        wrapper = (
-            "def __wrap():\n" f"{code}\n\n" "result_dict['result'] = __wrap()"
-        )
+        wrapper = "def __wrap():\n" f"{code}\n\n" "result_dict['result'] = __wrap()"
         result_dict = {}
         globals_dict["result_dict"] = result_dict
         exec(wrapper, globals_dict)
@@ -82,16 +98,18 @@ class DataPolice(models.Model):
 
     def _fetch_objects(self):
         self.ensure_one()
-        obj =self.env[self.model_id.model]
+        obj = self.env[self.model_id.model]
         if self.domain:
             instances = obj.search(self.domain)
         else:
-            instances = self.exec_get_result(self.fetch_expr, {'model': obj, 'obj': obj})
+            instances = self.exec_get_result(
+                self.fetch_expr, {"model": obj, "obj": obj}
+            )
         instances = instances.with_context(prefetch_fields=False)
 
     def _run_code(self, instance, expr):
         try:
-            result = self.exec_get_result(self.check_exp, {'obj': instance})
+            result = self.exec_get_result(self.check_exp, {"obj": instance})
             if result is None or result is True:
                 result = True
             else:
@@ -103,8 +121,8 @@ class DataPolice(models.Model):
             result = False
 
         return {
-            'ok': result,
-            'exception': exception,
+            "ok": result,
+            "exception": exception,
         }
 
     def _make_checks(self, instances):
@@ -113,20 +131,33 @@ class DataPolice(models.Model):
             _logger.debug(f"Checking {dp.name} {idx} of {len(objects)}")
             instance_name = self.env["data.police.formatter"].do_format(obj)
             res = self.run_code(obj, self.check_expr)
-            res['tried_to_fix'] = False
+            res["tried_to_fix"] = False
 
-            if not res['ok'] and self.fix_expr:
+            if not res["ok"] and self.fix_expr:
                 res_fix = self.run_code(obj, self.fix_expr)
-                res['tried_to_fix'] = True
-                res['fix_result'] = res_fix
+                res["tried_to_fix"] = True
+                res["fix_result"] = res_fix
 
-                if not res['ok'] and (not res['tried_to_fix'] or not res['fix_result']['ok']):
-                    text = u"; ".join(filter(bool, [instance_name, res.get('exception', ''), res.get('fix_result', {}).get('exception')]))
-                    _logger.error(f"Data Police {dp.name}: not ok at {obj._name} {obj.id} {text}")
+                if not res["ok"] and (
+                    not res["tried_to_fix"] or not res["fix_result"]["ok"]
+                ):
+                    text = "; ".join(
+                        filter(
+                            bool,
+                            [
+                                instance_name,
+                                res.get("exception", ""),
+                                res.get("fix_result", {}).get("exception"),
+                            ],
+                        )
+                    )
+                    _logger.error(
+                        f"Data Police {dp.name}: not ok at {obj._name} {obj.id} {text}"
+                    )
                     yield {
-                        'model': obj._name,
-                        'res_id': obj.id,
-                        'text': text,
+                        "model": obj._name,
+                        "res_id": obj.id,
+                        "text": text,
                     }
                     self.env.cr.commit()
 
@@ -142,7 +173,7 @@ class DataPolice(models.Model):
 
     def _get_all_email_recipients(self):
         def str2mails(s):
-            s = s or ''
+            s = s or ""
             s = s.replace(",", ";")
             return [x.lower() for x in s.split(";") if x]
 
@@ -151,9 +182,9 @@ class DataPolice(models.Model):
             if dp.recipients:
                 dp_recipients += str2mails(dp.recipients)
             if dp.user_ids:
-                dp_recipients += [x.lower() for x in dp.user_ids.mapped('email') if x]
+                dp_recipients += [x.lower() for x in dp.user_ids.mapped("email") if x]
 
-        mail_to = ','.join(set(dp_recipients))
+        mail_to = ",".join(set(dp_recipients))
 
         return mail_to
 
@@ -163,17 +194,23 @@ class DataPolice(models.Model):
         if not errors:
             name = "Success: #{self.name}"
             return name, name
-        text += u"<h2>{}</h2>".format(self.name)
+        text += "<h2>{}</h2>".format(self.name)
         text += "<ul>"
         small_text = text
-        for i, error in enumerate(sorted(errors, key=lambda e: (e.get('model', False), e.get('res_id', False)), reverse=True)):
-            if 'model' in error and 'res_id' in error:
-                url = self.env['ir.config_parameter'].get_param('web.base.url')
-                url += "#model=" + error['model'] + "&id=" + str(error['res_id'])
-                link = u"<a href='{}'>{}</a>".format(url, error['text'])
-                appendix = u"<li>{}</li>\n".format(link)
+        for i, error in enumerate(
+            sorted(
+                errors,
+                key=lambda e: (e.get("model", False), e.get("res_id", False)),
+                reverse=True,
+            )
+        ):
+            if "model" in error and "res_id" in error:
+                url = self.env["ir.config_parameter"].get_param("web.base.url")
+                url += "#model=" + error["model"] + "&id=" + str(error["res_id"])
+                link = "<a href='{}'>{}</a>".format(url, error["text"])
+                appendix = "<li>{}</li>\n".format(link)
             else:
-                appendix = u"<li>{}</li>\n".format(error)
+                appendix = "<li>{}</li>\n".format(error)
             text += appendix
             if i < 50:
                 small_text += appendix
@@ -183,48 +220,54 @@ class DataPolice(models.Model):
         return small_text, text
 
     def _send_mails(self):
-
         by_email = {}
         for dp in self:
             mail_to = dp._get_all_email_recipients()
             new_small_text, new_text = self._get_error_text()
 
             for email in mail_to.split(","):
-                by_email.setdefault(email, {'text': '', 'small_text': ''})
-                by_email[email]['text'] += new_text
-                by_email[email]['small_text'] += new_small_text
+                by_email.setdefault(email, {"text": "", "small_text": ""})
+                by_email[email]["text"] += new_text
+                by_email[email]["small_text"] += new_small_text
 
-        for email, texts in by_email.items):
-            if not texts['text']:
+        for email, texts in by_email.items():
+            if not texts["text"]:
                 continue
-            text = base64.encodestring(texts['text'].encode("utf-8"))
-            self.env["mail.mail"].create({
-                'auto_delete': True,
-                'subject': 'DataPolice Run {}'.format(datetime.now().strftime("%Y-%m-%d")),
-                'body_html': texts['small_text'],
-                'body': texts['small_text'],
-                'email_to': email,
-                'attachment_ids': [[0, 0, {
-                    'datas': text,
-                    'datas_fname': 'data_police.html',
-                    'name': 'data_police.html',
-                }]
-                ],
-            }).send()
+            text = base64.encodestring(texts["text"].encode("utf-8"))
+            self.env["mail.mail"].create(
+                {
+                    "auto_delete": True,
+                    "subject": f"DataPolice Run {datetime.now()}",
+                    "body_html": texts["small_text"],
+                    "body": texts["small_text"],
+                    "email_to": email,
+                    "attachment_ids": [
+                        [
+                            0,
+                            0,
+                            {
+                                "datas": text,
+                                "datas_fname": "data_police.html",
+                                "name": "data_police.html",
+                            },
+                        ]
+                    ],
+                }
+            ).send()
 
         return True
 
     def show_errors(self):
         obj = self.env[self.model_id.model]
         errors = json.loads(self.last_errors)
-        ids = [x['res_id'] for x in errors if 'res_id' in x]
+        ids = [x["res_id"] for x in errors if "res_id" in x]
 
         return {
-            'name': f"Errors of {self.name}",
-            'view_type': 'form',
-            'res_model': self._name,
-            'domain': [('id', 'in', ids)],
-            'views': [(False, 'tree'), (False, 'form')],
-            'type': 'ir.actions.act_window',
-            'target': 'current',
+            "name": f"Errors of {self.name}",
+            "view_type": "form",
+            "res_model": self._name,
+            "domain": [("id", "in", ids)],
+            "views": [(False, "tree"), (False, "form")],
+            "type": "ir.actions.act_window",
+            "target": "current",
         }
